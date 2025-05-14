4da1c5d4a6af698525722ff5e8d994bb358ec974
kaestro <didme07@gmail.coom>
2025-05-15 06:29:12 +0900

    Applied basic cmake build system
    - Added CMakeLists.txt
    - Separated Server.h and server.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "src/server.cpp"
+#include "include/Server.h"
 #include <iostream>
 
 int main() {
-  Server game_server(8080);
+  Server game_server;
   if (game_server.start()) {
-    std::cout << "서버가 시작되었습니다." << std::endl;
+    std::cout << "서버 로직이 (단일 클라이언트 처리 후) 완료되었습니다."
+              << std::endl;
   }
   game_server.stop();
 
